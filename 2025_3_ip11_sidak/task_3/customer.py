eec3b318c28fc3c06862b813003fb46555432e36
Kyryl Sidak <ksidak7@gmail.com>
2025-01-12 23:54:49 +0200

    Finish task_3 of ticket 2

--- 2025_3_ip11_sidak/task_3/customer.py
+++
@@ -1,3 +0,0 @@
-class Customer:
-    def __init__(self, enter_time):
-        self.enter_time = enter_time
--- 2025_3_ip11_sidak/task_3/route.py
+++
@@ -1,5 +0,0 @@
-class Route:
-    def __init__(self, element, p=None):
-        self.element = element
-        self.p = p
-
